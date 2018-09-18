856a7483c2b2bbe7d0298361410267cab6a5676a
jtrenaud1s <42702192+jtrenaud1s@users.noreply.github.com>
2018-09-18 00:16:34 -0500

    Update Main.java

--- src/me/jtrenaud1s/docgen/Main.java
+++ src/me/jtrenaud1s/docgen/Main.java
@@ -3,31 +3,21 @@ package me.jtrenaud1s.docgen;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
 import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        String srcDir = "";
-        String outputDir = "";
-        String outputFile = "";
-        ArrayList<File> outputImgs = new ArrayList<>();
+        String srcDir;
+        String outputDir;
+        String outputFile;
+        String projectDir;
         Settings settings = null;
         try {
             UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
             settings = new Settings();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        } catch (IOException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException | ClassNotFoundException e) {
             e.printStackTrace();
         }
 
@@ -35,37 +25,16 @@ public class Main {
         JFileChooser chooser = new JFileChooser();
 
         chooser.setCurrentDirectory(settings.getHomeDirectory());
-        chooser.setDialogTitle("Select the src folder in your project directory: ");
+        chooser.setDialogTitle("Select the your project's root directory: ");
         chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         chooser.showDialog(null, "Select");
-        if(chooser.getSelectedFile() == null) {
-            JOptionPane.showMessageDialog(chooser, "The program is closing, as you've cancelled the operation!");
-            return;
-        }
-        srcDir = chooser.getSelectedFile().getAbsolutePath();
-
-        outputFile = chooser.getSelectedFile().getParentFile().getName() + ".docx";
-
+        projectDir = chooser.getSelectedFile().getAbsolutePath();
+        srcDir = getSrcDir(projectDir);
+        outputFile = chooser.getSelectedFile().getName() + ".docx";
         chooser.setCurrentDirectory(chooser.getSelectedFile());
-
-        chooser.setDialogTitle("Select your output screenshot image(s): ");
-        chooser.addChoosableFileFilter(new FileNameExtensionFilter("PNG Files", "png"));
-        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        chooser.setMultiSelectionEnabled(true);
-        chooser.showDialog(null, "Select");
-        if(chooser.getSelectedFiles().length == 0) {
-            JOptionPane.showMessageDialog(chooser, "The program is closing, as you've cancelled the operation!");
-            return;
-        }
-
-        for (File f : chooser.getSelectedFiles()) {
-            outputImgs.add(f);
-        }
-
         outputDir = settings.getOutputDirectory().getAbsolutePath();
 
-
-        Project project = new Project(srcDir, outputImgs, outputDir, outputFile);
+        Project project = new Project(projectDir, srcDir, outputDir, outputFile);
 
         try {
             project.save();
@@ -74,5 +43,32 @@ public class Main {
         } catch (InvalidFormatException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String getSrcDir(String projectDir) {
+        File project = new File(projectDir);
+        for (File f : project.listFiles()) {
+            if (f.isDirectory()) {
+                if (f.getName().equalsIgnoreCase("src")) {
+                    for (File f2 : f.listFiles()) {
+                        if (f2.isDirectory()) {
+                            if (f2.getName().equalsIgnoreCase("main")) {
+                                for (File main : f2.listFiles()) {
+                                    if (main.isDirectory()) {
+                                        if (main.getName().equalsIgnoreCase("java")) {
+                                            System.out.println("Project is NetBeans");
+                                            return main.getAbsolutePath();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    System.out.println("Project is IntelliJ IDEA");
+                    return f.getAbsolutePath();
+                }
+            }
+        }
+        return "";
     }
 }
